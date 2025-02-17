e9f85708215e719e01af5e05426eabf727a30bd7
Макаренко <mahadrum@yandex.ru>
2025-02-17 23:59:06 +0300

    Добавьте файлы проекта.

---
+++ WebApplicationStart/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Tasks()
+        {
+            return View();
+        }
+
+        public string Task1()
+        {
+            return "Тут решение первой задачи";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
